3ddcf6951bb04d997f68c21a34e11d7bc88bc074
Adrian Muzyka <adrian.muzyka@consult.red>
2024-01-17 07:57:44 +0100

    Increased MAX_VMAS to 12288 and PIDs nbr limit to 48

--- memcr.c
+++ memcr.c
@@ -139,7 +139,7 @@ static int nr_threads;
 
 #define SERVICE_MODE_SELECT_TIMEOUT_MS	100
 
-#define MAX_VMAS			4096
+#define MAX_VMAS			(3*4096)
 static struct vm_area vmas[MAX_VMAS];
 static int nr_vmas;
 
@@ -183,7 +183,7 @@ int __attribute__((weak)) lib__write(int fd, const void *buf, size_t count);
 int __attribute__((weak)) lib__init(int enable, const char *arg);
 int __attribute__((weak)) lib__fini(void);
 
-#define CHECKPOINTED_PIDS_LIMIT 16
+#define CHECKPOINTED_PIDS_LIMIT 48
 #define PID_INVALID		0
 #define STATE_RESTORED			0
 #define STATE_CHECKPOINTING		1
